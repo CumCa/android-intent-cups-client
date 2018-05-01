064215225c26755a90429c65e8f4322ffc7976b3
Carine Chum <carine.chum@gmail.com>
2018-05-01 22:07:32 +0200

    Disable finish when touch outside

--- app/src/main/java/org/rainbrurpg/transparentcupsclient/MainActivity.java
+++ app/src/main/java/org/rainbrurpg/transparentcupsclient/MainActivity.java
@@ -67,6 +67,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+	setFinishOnTouchOutside(false);
         setContentView(R.layout.activity_main);
     }
 
